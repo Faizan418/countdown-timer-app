d31cea40de475b7ad0dd4c0599c7b55e4044014a
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-10-24 01:01:40 +0500

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,26 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
+
+  function toggleMenu() {
+    const stul = document.querySelector('ul');
+      stul?.classList.toggle('active');
+
+    }
+
   return (
     <div className='main-header-page'>
       <nav className='main-header-nav'>
         <div className='nav-logo'>
           <Image className='logo' src="/images/perfect-logo.png" width={120} height={120} alt='Logo'/>
         </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          &#9776;
+      </div>
+
         <div>
         <ul>
           <li><Link href={"/"}>Home</Link></li>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,18 @@
+'use client'
 import React from 'react'
 
 export default function Hero() {
+ function setime(){
+  let hour = prompt("Enter Number of Hours: ")
+  let minute = prompt("Enter Number of Minutes: ")
+  let second = prompt("Enter Number of seconds: ")
+    
+        document.getElementById("hours").innerHTML = hour;
+        document.getElementById("minutes").innerHTML = minute;
+        document.getElementById("seconds").innerHTML = second;
+
+    }
+
   return (
     <div className='main-hero-page'>
       
@@ -8,7 +20,7 @@ export default function Hero() {
         <h1>Count Down Timer</h1>
       </div>
 
-      <div className="time-box">
+        <div className="time-box">
 
             <div className="s-h" id="hours">00</div>
             <span className="double-dots">:</span>
@@ -17,10 +29,10 @@ export default function Hero() {
             <div className="s-s" id="seconds">00</div>
     
             <div className="set-time-box">
-                <button className="set-time" type="button">Set Time</button>
+                <button className="set-time" type="button" onClick={setime}>Set Time</button>
             </div>   
-                    </div>
-                    <div className="buttons">
+        </div>
+        <div className="buttons">
             <button className="btn-other" type="button">Start</button>
             <button className="btn-other" type="button">Pause</button>
             <button className="btn-other" type="button">Reset</button>
